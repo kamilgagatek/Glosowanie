aa8656589314d8ffc37191efbd0e4b2457329073
Kamil Gagatek <kamil.gagatek3@gmail.com>
2023-12-16 19:12:54 +0100

    Wymagane poprawki

---
+++ script.js
@@ -0,0 +1,70 @@
+var wynik1=0;
+var wynik2=0;
+var wynik3=0;
+var wynik4=0;
+var wynikirazem=0;
+
+function kandydat1() {
+    wynik1++;
+    wynikirazem++; 
+    var wysokosc1=Math.round((wynik1/wynikirazem)*100,1);
+    var wysokosc2=Math.round((wynik2/wynikirazem)*100,1);
+    var wysokosc3=Math.round((wynik3/wynikirazem)*100,1);
+    var wysokosc4=Math.round((wynik4/wynikirazem)*100,1);
+    document.getElementById("slupek1").style.height=wysokosc1 + '%';
+    document.getElementById("slupek2").style.height=wysokosc2 + '%';
+    document.getElementById("slupek3").style.height=wysokosc3 + '%';
+    document.getElementById("slupek4").style.height=wysokosc4 + '%';
+    document.getElementById("wynik1").innerHTML=wysokosc1 + "%" + " (" + wynik1 + ")";
+    document.getElementById("wynik2").innerHTML=wysokosc2 + "%" + " (" + wynik2 + ")";
+    document.getElementById("wynik3").innerHTML=wysokosc3 + "%" + " (" + wynik3 + ")";
+    document.getElementById("wynik4").innerHTML=wysokosc4 + "%" + " (" + wynik4 + ")";
+}
+function kandydat2() {
+    wynik2++;
+    wynikirazem++;
+    var wysokosc1=Math.round((wynik1/wynikirazem)*100,1);
+    var wysokosc2=Math.round((wynik2/wynikirazem)*100,1);
+    var wysokosc3=Math.round((wynik3/wynikirazem)*100,1);
+    var wysokosc4=Math.round((wynik4/wynikirazem)*100,1);
+    document.getElementById("slupek1").style.height=wysokosc1 + '%';
+    document.getElementById("slupek2").style.height=wysokosc2 + '%';
+    document.getElementById("slupek3").style.height=wysokosc3 + '%';
+    document.getElementById("slupek4").style.height=wysokosc4 + '%';
+    document.getElementById("wynik1").innerHTML=wysokosc1 + "%" + " (" + wynik1 + ")";
+    document.getElementById("wynik2").innerHTML=wysokosc2 + "%" + " (" + wynik2 + ")";
+    document.getElementById("wynik3").innerHTML=wysokosc3 + "%" + " (" + wynik3 + ")";
+    document.getElementById("wynik4").innerHTML=wysokosc4 + "%" + " (" + wynik4 + ")";
+}
+function kandydat3() {
+    wynik3++;
+    wynikirazem++;
+    var wysokosc1=Math.round((wynik1/wynikirazem)*100,1);
+    var wysokosc2=Math.round((wynik2/wynikirazem)*100,1);
+    var wysokosc3=Math.round((wynik3/wynikirazem)*100,1);
+    var wysokosc4=Math.round((wynik4/wynikirazem)*100,1);
+    document.getElementById("slupek1").style.height=wysokosc1 + '%';
+    document.getElementById("slupek2").style.height=wysokosc2 + '%';
+    document.getElementById("slupek3").style.height=wysokosc3 + '%';
+    document.getElementById("slupek4").style.height=wysokosc4 + '%';
+    document.getElementById("wynik1").innerHTML=wysokosc1 + "%" + " (" + wynik1 + ")";
+    document.getElementById("wynik2").innerHTML=wysokosc2 + "%" + " (" + wynik2 + ")";
+    document.getElementById("wynik3").innerHTML=wysokosc3 + "%" + " (" + wynik3 + ")";
+    document.getElementById("wynik4").innerHTML=wysokosc4 + "%" + " (" + wynik4 + ")";
+}
+function kandydat4() {
+    wynik4++;
+    wynikirazem++;
+    var wysokosc1=Math.round((wynik1/wynikirazem)*100,1);
+    var wysokosc2=Math.round((wynik2/wynikirazem)*100,1);
+    var wysokosc3=Math.round((wynik3/wynikirazem)*100,1);
+    var wysokosc4=Math.round((wynik4/wynikirazem)*100,1);
+    document.getElementById("slupek1").style.height=wysokosc1 + '%';
+    document.getElementById("slupek2").style.height=wysokosc2 + '%';
+    document.getElementById("slupek3").style.height=wysokosc3 + '%';
+    document.getElementById("slupek4").style.height=wysokosc4 + '%';
+    document.getElementById("wynik1").innerHTML=wysokosc1 + "%" + " (" + wynik1 + ")";
+    document.getElementById("wynik2").innerHTML=wysokosc2 + "%" + " (" + wynik2 + ")";
+    document.getElementById("wynik3").innerHTML=wysokosc3 + "%" + " (" + wynik3 + ")";
+    document.getElementById("wynik4").innerHTML=wysokosc4 + "%" + " (" + wynik4 + ")";
+}
